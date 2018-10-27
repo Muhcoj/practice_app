c68b85b91875032cea47fa2b245b5a6a797fcf66
Muhcoj <jochum.de.bueger@gmail.com>
2018-10-27 20:02:17 +0200

    Added flash message successful user save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcompe to the Practice App"
       redirect_to @user
     else
       render 'new'
