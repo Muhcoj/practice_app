e4e8fcf10880e4e7c93afb9bacf192ce1cc02809
Muhcoj <jochum.de.bueger@gmail.com>
2018-10-27 20:39:03 +0200

    A test for valid submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save 
       flash[:success] = "Welcompe to the Practice App"
       redirect_to @user
     else
