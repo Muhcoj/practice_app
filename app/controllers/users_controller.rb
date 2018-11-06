b3102b7cc3db7c0ea15332893ea971e945409e8c
Muhcoj <jochum.de.bueger@gmail.com>
2018-11-06 20:26:32 +0100

    Implemented friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
@@ -54,7 +55,7 @@ class UsersController < ApplicationController
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user(@user) 
+      redirect_to(root_url) unless current_user?(@user) 
     end
 
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,7 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test 'unsuccessfil edit' do
+  test 'unsuccessful edit' do
     log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
@@ -34,5 +34,25 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
+  end
+
+  test 'Successful edit with friendly forwarding' do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+    assert_nil session[:forwarding_url]
+    log_in_as(@user)
+    assert_redirected_to user_url(@user)
   end
 end
