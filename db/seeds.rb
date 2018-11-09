46b9447f9cee9fe9a69c68f3291cc27ea85b45a6
Muhcoj <jochum.de.bueger@gmail.com>
2018-11-09 19:50:15 +0100

    Added admin attribute to user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 User.create!(name:  "Example User", 
              email: "example@railstutorial.org",
              password:              "asdfasdf", 
-             password_confirmation: "asdfasdf")
+             password_confirmation: "asdfasdf",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
