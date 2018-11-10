fc715fcd940807de94759653d3668792ef5e1663
Muhcoj <jochum.de.bueger@gmail.com>
2018-11-10 20:37:49 +0100

    Generated User migration for activation account and updated seeds and fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -8,6 +8,7 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  before_create :create_activation_digest
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -37,6 +38,19 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+
+    # Converts email to all lower-case
+    def downcase_email
+      email.downcase!
+    end
+
+    # Creates and assigns the activation token and digest
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 end
  
